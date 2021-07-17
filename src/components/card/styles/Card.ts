d12349af33e8042ba784973e49c34c1c6af07207
pixelts <ahmed.zaki.bennecer@gmail.com>
2021-07-17 23:37:41 +0100

    feat: end with responsive

--- src/components/card/styles/Card.ts
+++ src/components/card/styles/Card.ts
@@ -1,8 +1,13 @@
 import styled from "styled-components/macro";
+import { media } from "../../../styles/global-style";
 import {
   neutralDarkBlue,
   neutralGrayshBlue,
   neutralLightGray,
+  primaryBlue,
+  primaryCyan,
+  primaryOrange,
+  primaryRed,
 } from "../../../styles/style-guide-var";
 
 export const Section = styled.section`
@@ -12,12 +17,20 @@ export const Section = styled.section`
 
   background-color: ${neutralLightGray};
   padding-top: 66px;
+
+  ${media.desktop} {
+    padding-top: 52px;
+  }
 `;
 
 export const Container = styled.div`
   width: 40%;
   min-width: 312px;
   text-align: center;
+
+  ${media.desktop} {
+    width: min(1400px, 60%);
+  }
 `;
 
 export const Title = styled.h2`
@@ -31,23 +44,49 @@ export const Title = styled.h2`
     letter-spacing: 0.8px;
     font-size: 1.5rem;
   }
+
+  ${media.desktop} {
+    line-height: 3.3rem;
+    font-size: 2.4rem;
+
+    span {
+      font-size: 2.3rem;
+    }
+  }
 `;
 
 export const Description = styled.p`
   line-height: 1.7rem;
   font-size: 1.01rem;
   color: ${neutralGrayshBlue};
+
+  ${media.desktop} {
+    margin: 18px auto;
+    max-width: 510px;
+  }
 `;
 
 export const Group = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: 74px;
+
+  ${media.desktop} {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 63px;
+  }
 `;
 
 export const Pane = styled.div`
   display: flex;
   flex-direction: column;
+
+  ${media.desktop} {
+    flex: 1;
+    padding: 0 18px;
+  }
 `;
 export const Item = styled.div<{ clr: string }>`
   height: 222px;
@@ -63,6 +102,16 @@ export const Item = styled.div<{ clr: string }>`
   padding: 25px;
   margin-bottom: 24px;
   box-shadow: 0px 12px 40px -20px ${neutralGrayshBlue};
+
+  ${media.desktop} {
+    height: 250px;
+    margin-bottom: 28px;
+    padding: 28px;
+
+    ${({ clr }) => clr === primaryCyan && "padding: 30px"}
+    ${({ clr }) => clr === primaryBlue && "padding: 30px 24px"}
+    ${({ clr }) => clr === primaryOrange && "padding: 30px 28px"}
+  }
 `;
 
 export const CardTitle = styled.h4`
@@ -76,6 +125,11 @@ export const CardInfos = styled.p`
   font-size: 0.88rem;
   line-height: 1.48rem;
   color: ${neutralGrayshBlue};
+
+  ${media.desktop} {
+    margin-top: 11px;
+    font-size: 0.87rem;
+  }
 `;
 
 export const IconContainer = styled.div`
@@ -95,4 +149,20 @@ export const CardIcon = styled.div<{ src?: string }>`
   background-repeat: no-repeat;
   background-position: left bottom;
   margin-right: 4px;
+
+  ${media.desktop} {
+    width: 63px;
+
+    ${({ src }) =>
+      src === "/card-feature-section/images/icon-supervisor.svg" &&
+      "margin-right: -1px; background-position: bottom -1px right -1px"}
+
+    ${({ src }) =>
+      src === "/card-feature-section/images/icon-calculator.svg" &&
+      "margin-right: 12px; margin-bottom: -2px"}
+
+    ${({ src }) =>
+      src === "/card-feature-section/images/icon-karma.svg" &&
+      "margin-bottom: -4px"}
+  }
 `;
